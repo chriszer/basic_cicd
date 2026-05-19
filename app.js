13f87eda2d10244c9d2f8659b7fab23e548aaa33
Chriszer Myth G. Tamayo <chriszer@Chriszers-MacBook-Pro.local>
2026-05-19 23:30:47 +0800

    let msg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const msg = 'Works on my machine.';
+  let msg = 'Works on my machine.';
 
   res.send(msg);
 });
